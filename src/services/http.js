816653227123d9c4f114fa3c59bc853fb8041e7b
Gautier MOREL <gautier.morel@outlook.com>
2022-02-13 23:14:25 +0100

    chore: release

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import store from "@/store";
-// import { ElNotification } from 'element-plus';
 
 export default function fetchApi(secured = true) {
 	if (secured) {
@@ -14,7 +13,9 @@ export default function fetchApi(secured = true) {
 
 		api.interceptors.response.use((response) => response, (error) => {
 			let { data = '', status = 500, statusText = 'Une erreur inattendue s’est produite' } = (error && error.response) || {};
-			// ElNotification.error({ title: 'Erreur', message: `${status} - ${statusText} ${typeof data === 'string' ? data : ''}` });
+			console.log('data:', data)
+			console.log('statusText:', statusText)
+      // ElNotification.error({ title: 'Erreur', message: `${status} - ${statusText} ${typeof data === 'string' ? data : ''}` });
 			return Promise.reject(status);
 		});
 
