0ca64f506ee900a1bf3e4887c04887f4ca02c825
Gautier MOREL <gautier.morel@outlook.com>
2022-04-22 00:38:08 +0200

    chore: release

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,20 +4,22 @@
 			<Divider class="w-6" />
 		</div>
 		<div class="flex flex-column align-items-center">
+			<p><a href="https://www.millemercismariage.com/21mai2022/liste.html" target="blank">https://www.millemercismariage.com/21mai2022/liste.html</a></p>
 			<p class="footer__copyright mb-0">© 2022 - 21mai.be · <a class="footer__link md-0" href="#" @click="show=!show">Liste de mariage</a></p>
 			<a v-if="isAuthenticated" href="#" @click="onLogout">Se déconnecter</a>
 		</div>
 		<Dialog v-model:visible="show">
 			<template #header>
-				Encore un petit instant...
+				♡
 			</template>
 
-			<div>Nous sommes entrain de constituer notre liste de mariage :)</div>
+			<div>Si vous comptez nous aider, nous vous proposons de le faire grâce à notre liste de mariage</div>
+      <div>Comme nous ne comptons pas déménager avant quelques temps, nous avons fait le choix d'y mettre principalement des activités liées à notre voyage de Noces !</div>
 			<div>Si vous preferez nous aider en nous apportant une contribution libre, voilà nos identifiants bancaires:</div>
 
 			<pre style="text-align:center">BE34001885252590</pre>
 
-      <div>Merci !! :)</div>
+			<div>Merci !! :)</div>
 
 			<template #footer>
 				<Button label="D'accord" class="m-1" @click="show = false" />
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,11 @@
 <template>
+	<div class="p-3 message-list">
+		<Message severity="warn" icon="info">
+			<p>Nous avons eu beaucoup de demandes concernant notre liste de mariage: Même si notre plus beau cadeau sera de vous avoir près de nous le jour J, notre liste de mariage est enfin disponible ici: <a target="_blank" href="https://www.millemercismariage.com/21mai2022/liste.html">millemercismariage</a></p>
+			<p>Si vous preferez nous participer de façon libre, c'est aussi possible par virement sur notre compte commun: <b>BE34001885252590</b> <small>(N'oubliez pas de nous dire qui vous êtes pour que nous puissions vous remercier 😀 )</small></p>
+			<p>Merci ! 😍 Gautier & AnnSo</p>
+		</Message>
+	</div>
 	<div class="flex flex-wrap align-items-center justify-content-center">
 		<Headline />
 
@@ -15,7 +22,7 @@ import LoginForm from "@/components/LoginForm.vue";
 export default {
 	name: "Login",
 	components: {
-    Headline,
+		Headline,
 		LoginForm,
 	},
 	computed: {
@@ -23,3 +30,14 @@ export default {
 	}
 };
 </script>
+
+<style scoped lang="scss">
+.message-list {
+	p {
+		font-size: 0.8rem;
+	}
+	a {
+		font-size: 1rem;
+	}
+}
+</style>
